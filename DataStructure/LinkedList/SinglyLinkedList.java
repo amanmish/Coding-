36e4e611133e4bfcb6648699a79084666309a11d
mishraaman <aman.kumar.mishra1996@gmail.com>
2018-04-03 13:59:50 +0530

    code-code-code

--- DataStructure/LinkedList/SinglyLinkedList.java
+++ DataStructure/LinkedList/SinglyLinkedList.java
@@ -1,2 +1,36 @@
 import java.util.*;
 import java.lang.*;
+class Linked_List
+{
+	Node head;
+	static class Node
+	{
+		int data;
+		Node next;
+		Node(int d)
+		{
+			data=d;
+			next=null;
+		}
+	}
+	public void printList()
+	{
+		Node ptr=head;
+		while(ptr!=null)
+		{
+			System.out.print(ptr.data+"  ");
+			ptr=ptr.next;
+		}
+	}
+	public static void main(String ar[])
+	{
+		Linked_List list=new Linked_List();
+		list.head=new Node(12);
+		Node s=new Node(24);
+		Node t=new Node(36);
+		list.head.next=s;
+		s.next=t;
+		list .printList();
+	}
+}
+	
